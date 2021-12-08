d6ec72c1090ec2e0b4f2e02ebd0168a877df4ee6
Pratik Singh <31792153+pratiksingh05@users.noreply.github.com>
2021-12-08 21:04:18 +0530

    Update install_essentials.sh

--- install_essentials.sh
+++ install_essentials.sh
@@ -1,25 +1,25 @@
 #!/bin/bash -x
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
+    sudo apt-get update -y && sleep 100s
+    sudo apt-get upgrade -y && sleep 210s
 #To resolve unable to initialize frontend: Dialog error
     echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
     sudo apt-get install dialog apt-utils
-    sudo apt-get install -y -q
+    sudo apt-get install -y -q && sleep 40s
     sudo chmod 777 /var/cache/debconf/
     sudo chmod 777 /var/cache/debconf/passwords.dat
-    apt-get install -y gnupg2
+    sudo apt-get install -y gnupg2 && sleep 50s
 #Added command from article - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2rl_verify.html
     curl -O https://s3.amazonaws.com/ec2rescuelinux/ec2rl.key
     gpg2 --import ec2rl.key
     curl -O https://s3.amazonaws.com/ec2rescuelinux/ec2rl.tgz.sig
     gpg2 --verify ./ec2rl.tgz.sig
 #Install open jdk
-    sudo apt-get install openjdk-11-jdk -y && sleep 60
+    sudo apt-get install openjdk-11-jdk -y && sleep 60s
     echo "---------------------------------------------------------"
     echo "JDK installation completed."
     echo "---------------------------------------------------------"
 #Install python pip3, python and nfs modules
-    sudo apt-get -y install python3-pip
+    sudo apt-get -y install python3-pip && sleep 60s
     sudo apt-get install -y build-essential libffi-dev python-dev
     sudo apt-get install -y nfs-common
     sudo systemctl restart systemd-logind
@@ -28,16 +28,16 @@
     echo "Python3 bycrypt module install completed.."
     echo "---------------------------------------------------------"
 #Install Maven:
-    sudo apt-get install maven -y
+    sudo apt-get install maven -y && sleep 60s
 #Step-8: Install Docker:
     sudo apt-get install ca-certificates curl gnupg lsb-release -y
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update -y
+    sudo apt-get update -y && sleep 30s
     sudo apt-get install docker-ce docker-ce-cli containerd.io -y    
 #Step-9: Install NodeJS:
-    sudo apt install nodejs npm -y
+    sudo apt install nodejs npm -y && sleep 50s
 #Step-10: Intall Git:
-    sudo apt install git-all -y
+    sudo apt install git-all -y && sleep 60s
 #Step-11: Install Zip:
-    sudo apt install zip unzip -y
+    sudo apt install zip unzip -y && sleep 60s
