017e7e3b0cb29c596b20b8a1c326902ab5acb75f
Pratik Singh <31792153+pratiksingh05@users.noreply.github.com>
2021-12-09 00:43:28 +0530

    Update install_essentials.sh

--- install_essentials.sh
+++ install_essentials.sh
@@ -31,7 +31,7 @@
 #     sudo apt-get install ca-certificates curl gnupg lsb-release -y
 #     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 #     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#     sudo apt-get update -y && sleep 30
+##     sudo apt-get update -y && sleep 30
 #     sudo apt-get install docker-ce docker-ce-cli containerd.io -y    
 # #Step-9: Install NodeJS:
       sudo apt install nodejs npm -y && \
