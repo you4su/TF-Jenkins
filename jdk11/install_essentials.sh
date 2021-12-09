4217e79d1529d643b86ac8b837cf37710b65c860
Pratik Singh <31792153+pratiksingh05@users.noreply.github.com>
2021-12-09 19:17:19 +0530

    Create install_essentials.sh

---
+++ jdk11/install_essentials.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash -x
+
+sudo apt-get update -y && sudo apt-get upgrade -y \
+echo "updrage done"
+
+#Debconf
+  sudo chmod 777 /home/ubuntu
+  echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections && \
+  echo 'config successfull' & \
+  sudo apt-get install dialog apt-utils -y && sudo apt-get install -y -q && \
+  sudo chmod 777 /var/cache//debconf/ && sudo chmod 777 /var/cache/debconf/passwords.dat && \
+  sudo apt-get install -y gnupg2 && \
+  ls -la /home/ubuntu
+#Added command
+  curl -O https://s3.amazonaws.com/ec2rescuelinux/ec2rl.key && \
+  gpg2 --imort ec2rl.key && \
+  curl -O https://s3.amazonaws.com/ec2rescuelinux/ec2rl.tgz.sig && \
+  gpg2 --verify ./ec2rl.tgz.sig
+#Install open jdk
+#  sudo apt-get install openjdk-11-jdk -y \
+#  echo "------------------------------"
+#  echo "JDK install done."
+#Install Amazon Corretto
+   wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -
+   sudo add-apt-repository 'deb https://apt.corretto.aws stable main' && \
+   sudo apt-get update; sudo apt-get install -y java-11-amazon-corretto-jdk && \
+   echo "---------------------------------------------"
+   echo "Amazon Correctto installation completed."
+#Install python pip3
+  sudo apt-get -y install python3-pip && sudo apt-get install -y build-essential libffi-dev-python-dev && \
+  sudo apt-get install -y nfs-common && sudo systemctl restart systemd-logind && \
+  pip3 install python-bcrypt -y \
+  echo "Python 3 install done"
+
+#Install NodeJS
+  sudo apt-get install nodejs npm -y \
+#Install Git
+  sudo apt-get install git-all -y \
+#Install zip
+  sudo apt-get install zip -y
