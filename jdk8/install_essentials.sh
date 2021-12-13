f87a7b1de58d5bf53583b4cd16cce0d681959233
Pratik Singh <31792153+pratiksingh05@users.noreply.github.com>
2021-12-13 16:08:03 +0530

    Update install_essentials.sh

--- jdk8/install_essentials.sh
+++ jdk8/install_essentials.sh
@@ -29,6 +29,7 @@ echo "updrage done"
 #Install NodeJS
   #sudo apt-get install nodejs npm -y \ return 8.5v
   curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && \
+  sudo apt-get install nodejs -y && \
   curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null && \
   echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && \
   sudo apt-get update -y && \
