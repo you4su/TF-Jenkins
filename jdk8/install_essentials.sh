4562536878db502ee5e7f608ee9cd701878940cd
Pratik Singh <31792153+pratiksingh05@users.noreply.github.com>
2021-12-13 15:27:31 +0530

    Update install_essentials.sh

--- jdk8/install_essentials.sh
+++ jdk8/install_essentials.sh
@@ -17,26 +17,26 @@ echo "updrage done"
   curl -O https://s3.amazonaws.com/ec2rescuelinux/ec2rl.tgz.sig && \
   gpg2 --verify ./ec2rl.tgz.sig
 #Install open jdk
- sudo apt-get install openjdk-11-jdk -y \
+ sudo apt-get install openjdk-11-jdk -y && \
  echo "------------------------------"
  echo "JDK install done."
+ sudo apt-get install gcc g++ make && \
 #Install python pip3
   sudo apt-get -y install python3-pip && sudo apt-get install -y build-essential libffi-dev-python-dev && \
   sudo apt-get install -y nfs-common && sudo systemctl restart systemd-logind && \
   pip3 install python-bcrypt -y \
   echo "Python 3 install done"
-
 #Install NodeJS
   #sudo apt-get install nodejs npm -y \ return 8.5v
   curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && \
-  sudo apt-get install nodejs npm -y  && sudo npm install yarn --global \
+  sudo apt-get install nodejs npm -y  && sudo npm install yarn --global && \
 #Install Git
-  sudo apt-get install git-all -y \
+  sudo apt-get install git-all -y && \
 #Install zip
-  sudo apt-get install zip -y
+  sudo apt-get install zip -y && \
 #Install AWS Cli
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
   unzip awscliv2.zip && \
   sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin && \
 #Install Angular cli
-  sudo npm install -g @angular/cli@11.1.2 
+  sudo npm install -g @angular/cli@11.1.2
