998db6b20bf539dd325782139f649a2c39cc9566
Pratik Singh <31792153+pratiksingh05@users.noreply.github.com>
2021-12-13 16:00:17 +0530

    Update install_essentials.sh

--- jdk8/install_essentials.sh
+++ jdk8/install_essentials.sh
@@ -32,7 +32,7 @@ echo "updrage done"
   curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null && \
   echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && \
   sudo apt-get update -y && \
-  sudo apt-get install nodejs npm -y && sudo npm install yarn --global -y && \
+  sudo apt-get install npm -y && sudo npm install yarn --global -y && \
 #Install Git
   sudo apt-get install git-all -y && \
 #Install zip
